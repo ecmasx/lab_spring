f0cfa40051dd8ad0f561adece012f12bc91e734d
Maxim Madan <maximmadan3@gmail.com>
2025-03-01 12:08:59 +0200

    fix

--- lab_2/src/main/java/com/example/lab_2/IntraJurnal.java
+++ lab_2/src/main/java/com/example/lab_2/IntraJurnal.java
@@ -1,54 +1,23 @@
 package com.example.lab_2;
 
-import lombok.Getter;
-import lombok.Setter;
-
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import java.time.LocalDate;
-import java.util.Objects;
 
-@Setter
-@Getter
-class IntraJurnal {
+@Data
+@NoArgsConstructor
+public class IntraJurnal {
     private LocalDate data;
     private String titlu;
     private String continut;
     private String stareEmotionala;
     private int nivelEnergie;
+    private Utilizator utilizator;
 
-    public IntraJurnal(LocalDate data, String titlu, String continut, String stareEmotionala, int nivelEnergie) {
-        this.data = data;
-        this.titlu = titlu;
-        this.continut = continut;
-        this.stareEmotionala = stareEmotionala;
-        this.nivelEnergie = nivelEnergie;
-    }
-
-    public IntraJurnal() {
-        this.data = LocalDate.now();
-    }
-
-
-    @Override
-    public String toString() {
-        return "IntraJurnal{" +
-                "data=" + data +
-                ", titlu='" + titlu + '\'' +
-                ", continut='" + continut + '\'' +
-                ", stareEmotionala='" + stareEmotionala + '\'' +
-                ", nivelEnergie=" + nivelEnergie +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        IntraJurnal that = (IntraJurnal) o;
-        return nivelEnergie == that.nivelEnergie && Objects.equals(data, that.data) && Objects.equals(titlu, that.titlu) && Objects.equals(continut, that.continut) && Objects.equals(stareEmotionala, that.stareEmotionala);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(data, titlu, continut, stareEmotionala, nivelEnergie);
+    @Autowired
+    private IntraJurnal(@Qualifier("utilizatorBean") Utilizator utilizator) {
+        this.utilizator = utilizator;
     }
 }
